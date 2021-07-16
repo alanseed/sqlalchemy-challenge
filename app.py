3c0414f740b4b383353a078196b1d6429e5d3891
alan.seed <alan.seed@gmail.com>
2021-07-16 12:13:56 +1000

    home done on app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-# starter file
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+app_dict = {"precipitation", "stations", "tobs", "<start>", "<start>/<end>"}
+
+@app.route("/")
+def home():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end"
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
+   
